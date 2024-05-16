d1a11186792dcdb2b173104f957679d962693911
b-kaloferov <boriskaloferov_j20@schoolmath.eu>
2024-05-16 08:59:26 +0300

    StorageDbContext class added that inherits DbContext.

---
+++ CodeFirst-StorageControlApplication/DataLayer/StorageDbContext.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class StorageDbContext : DbContext
+    {
+        public StorageDbContext() : base()
+        {
+            
+        }
+
+        public StorageDbContext(DbContextOptions options) : base(options) 
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-UNHGGLSQ\\SQLEXPRESS;Database=StorageDb;Trusted_Connection=True;TrustServerCertificate=True;");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Model> Models { get; set; }
+        public DbSet<Shoe> Shoes { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}

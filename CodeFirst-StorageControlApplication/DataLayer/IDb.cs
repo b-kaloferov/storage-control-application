31d3f67a456d1fdd3f26a48f873c341e5f4d2a47
b-kaloferov <boriskaloferov_j20@schoolmath.eu>
2024-05-16 10:21:53 +0300

    Interface IDb for the CRUD created

---
+++ CodeFirst-StorageControlApplication/DataLayer/IDb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public interface IDb<T, K>
+    {
+        Task CreateAsync(T entity);
+
+        Task<T> ReadAsync(K key, bool useNavigationalProperties = false, bool isReadOnly = true);
+
+        Task<List<T>> ReadAllAsync(bool useNavigationalProperties = false, bool isReadOnly = true);
+
+        Task UpdateAsync(T entity, bool useNavigationalProperties = false);
+
+        Task<T> DeleteAsync(K key);
+    }
+}

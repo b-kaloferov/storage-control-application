4c277a5256d5aa7d50f8afeb55b1d6ba3e108a1b
Dimitar25 <dimitar.bazlyankov@gmail.com>
2024-05-16 10:48:05 +0300

    Implemented CRUD for ModelsDbSet

---
+++ CodeFirst-StorageControlApplication/DataLayer/ModelsContext.cs
@@ -0,0 +1,125 @@
+﻿using BusinessLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class ModelsContext : IDb<Model, int>
+    {
+        private readonly StorageDbContext _storageDbContext;
+
+        public ModelsContext(StorageDbContext storageDbContext)
+        {
+            _storageDbContext = storageDbContext;
+        }
+        public async Task CreateAsync(Model entity)
+        {
+            try
+            {
+                _storageDbContext.Models.Add(entity);
+                await _storageDbContext.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<Model> ReadAsync(int key, bool useNavigationalProperties = false, bool isReadOnly = true)
+        {
+            try
+            {
+                IQueryable<Model> query = _storageDbContext.Models;
+
+                if (useNavigationalProperties)
+                {
+                    query = query.Include(e => e.Shoes);
+                }
+
+                if (isReadOnly)
+                {
+                    query = query.AsNoTrackingWithIdentityResolution();
+                }
+
+                return await query.SingleOrDefaultAsync(e => e.Id == key);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<List<Model>> ReadAllAsync(bool useNavigationalProperties = false, bool isReadOnly = true)
+        {
+            try
+            {
+                IQueryable<Model> query = _storageDbContext.Models;
+
+                if (useNavigationalProperties)
+                {
+                    query = query.Include(e => e.Shoes);
+                }
+
+                if (isReadOnly)
+                {
+                    query = query.AsNoTrackingWithIdentityResolution();
+                }
+
+                return await query.ToListAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task UpdateAsync(Model entity, bool useNavigationalProperties = false)
+        {
+            try
+            {
+                Model modelFromDb = await ReadAsync(entity.Id, false, false);
+
+                if (modelFromDb is null)
+                {
+                    throw new ArgumentException("Model with id = " + entity.Id + "does not exist!");
+                }
+
+                _storageDbContext.Entry(modelFromDb).CurrentValues.SetValues(entity);
+
+                await _storageDbContext.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task DeleteAsync(int key)
+        {
+            try
+            {
+                Model modelFromDb = await ReadAsync(key, false, false);
+                
+                if (modelFromDb is null)
+                {
+                    throw new ArgumentException("Model with id = " + key + "does not exist!");
+                }
+
+                _storageDbContext.Models.Remove(modelFromDb);
+                await _storageDbContext.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}

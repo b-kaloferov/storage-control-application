c2b9ab93b085c53a0a9699025e3e872725e56321
b-kaloferov <boriskaloferov_j20@schoolmath.eu>
2024-06-06 14:18:51 +0300

    IModelService and ModelService created

---
+++ CodeFirst-StorageControlApplication/ServiceLayer/IModelService.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer
+{
+    public interface IModelService
+    {
+        Task CreateModelAsync(Model model);
+        Task<Model> GetModelByIdAsync(int id, bool useNavigationalProperties = false);
+        Task<List<Model>> GetAllModelsAsync(bool useNavigationalProperties = false);
+        Task UpdateModelAsync(Model model, bool useNavigationalProperties = false);
+        Task DeleteModelAsync(int id);
+    }
+}
---
+++ CodeFirst-StorageControlApplication/ServiceLayer/ModelService.cs
@@ -0,0 +1,57 @@
+﻿using BusinessLayer;
+using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer
+{
+    public class ModelService : IModelService
+    {
+
+        private readonly ModelsContext _modelsContext;
+
+        public ModelService(ModelsContext modelsContext)
+        {
+            _modelsContext = modelsContext;
+        }
+
+        public async Task CreateModelAsync(Model model)
+        {
+            if (model == null)
+            {
+                throw new ArgumentNullException("There is not implemented Model object.");
+            }
+
+            await _modelsContext.CreateAsync(model);
+        }
+
+        public async Task<Model> GetModelByIdAsync(int id, bool useNavigationalProperties = false)
+        {
+            return await _modelsContext.ReadAsync(id, useNavigationalProperties);
+        }
+        
+        public async Task<List<Model>> GetAllModelsAsync(bool useNavigationalProperties = false)
+        {
+            return await _modelsContext.ReadAllAsync(useNavigationalProperties);
+        }
+
+        public async Task UpdateModelAsync(Model model, bool useNavigationalProperties = false)
+        {
+            if (model == null)
+            {
+                throw new ArgumentNullException("There is not implemented Model object.");
+            }
+
+            await _modelsContext.UpdateAsync(model, useNavigationalProperties);
+        }
+        
+        public async Task DeleteModelAsync(int id)
+        {
+            await _modelsContext.DeleteAsync(id);
+        }
+
+    }
+}

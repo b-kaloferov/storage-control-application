6f68a417595076728481795357969620c8381dfe
Dimitar25 <dimitar.bazlyankov@gmail.com>
2024-06-06 16:27:21 +0300

    IClientService and ClientService created

---
+++ CodeFirst-StorageControlApplication/ServiceLayer/ClientService.cs
@@ -0,0 +1,56 @@
+﻿using BusinessLayer;
+using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer
+{
+    public class ClientService : IClientService
+    {
+
+        private readonly ClientsContext _clientsContext;
+
+        public ClientService(ClientsContext clientsContext)
+        {
+            _clientsContext = clientsContext;
+        }
+
+        public async Task CreateClientAsync(Client client)
+        {
+            if (client == null)
+            {
+                throw new ArgumentNullException("There is not implemented Client object.");
+            }
+
+            await _clientsContext.CreateAsync(client);
+        }
+
+        public async Task<Client> GetClientByIdAsync(int id, bool useNavigationalProperties = false)
+        {
+            return await _clientsContext.ReadAsync(id, useNavigationalProperties);
+        }
+
+        public async Task<List<Client>> GetAllClientsAsync(bool useNavigationalProperties = false)
+        {
+            return await _clientsContext.ReadAllAsync(useNavigationalProperties);
+        }
+
+        public async Task UpdateClientAsync(Client client, bool useNavigationalProperties = false)
+        {
+            if (client == null)
+            {
+                throw new ArgumentNullException("There is not implemented Client object.");
+            }
+
+            await _clientsContext.UpdateAsync(client, useNavigationalProperties);
+        }
+        
+        public async Task DeleteClientAsync(int id)
+        {
+            await _clientsContext.DeleteAsync(id);
+        }
+    }
+}
---
+++ CodeFirst-StorageControlApplication/ServiceLayer/IClientService.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer
+{
+    public interface IClientService
+    {
+        Task CreateClientAsync(Client client);
+        Task<Client> GetClientByIdAsync(int id, bool useNavigationalProperties = false);
+        Task<List<Client>> GetAllClientsAsync(bool useNavigationalProperties = false);
+        Task UpdateClientAsync(Client client, bool useNavigationalProperties = false);
+        Task DeleteClientAsync(int id);
+    }
+}

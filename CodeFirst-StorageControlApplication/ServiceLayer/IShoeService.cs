2f72653e96c649a049b5f9f2c2f7e8ad49bc5f45
b-kaloferov <boriskaloferov_j20@schoolmath.eu>
2024-06-06 14:34:04 +0300

    IShoeService and ShoeService created

---
+++ CodeFirst-StorageControlApplication/ServiceLayer/IShoeService.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer
+{
+    public interface IShoeService
+    {
+        Task CreateShoeAsync(Shoe shoe);
+        Task DiscardShoesAsync(int id, int quantity);
+    }
+}
---
+++ CodeFirst-StorageControlApplication/ServiceLayer/ShoeService.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLayer;
+using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer
+{
+    public class ShoeService : IShoeService
+    {
+        private readonly ShoesContext _shoesContext;
+
+        public ShoeService(ShoesContext shoesContext)
+        {
+            _shoesContext = shoesContext;
+        }
+
+        public async Task CreateShoeAsync(Shoe shoe)
+        {
+            if (shoe == null)
+            {
+                throw new ArgumentNullException("There is not implemented Shoe object.");
+            }
+
+            await _shoesContext.CreateAsync(shoe);
+        }
+
+        public async Task DiscardShoesAsync(int id, int quantity)
+        {
+            var shoes = await _shoesContext.ReadAllAsync();
+            var shoesToDiscard = shoes.Where(s => s.Id == id).Take(quantity).ToList();
+
+            foreach (var shoe in shoesToDiscard)
+            {
+                await _shoesContext.DeleteAsync(shoe.Id);
+            }
+        }
+    }
+}

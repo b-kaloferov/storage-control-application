715c2c8c7f7774c9efc81d8bbc25f41f6c117a6a
b-kaloferov <boriskaloferov_j20@schoolmath.eu>
2024-06-17 22:02:52 +0300

    UpdateShoeModel form created and planned to be continued

---
+++ CodeFirst-StorageControlApplication/GraphicalUserInterface/UpdateShoeModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicalUserInterface
+{
+    public partial class UpdateShoeModel : Form
+    {
+        public UpdateShoeModel()
+        {
+            InitializeComponent();
+        }
+    }
+}

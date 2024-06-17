ed7ced52b73f4d69cc1dc88dd24118b97696efc5
b-kaloferov <boriskaloferov_j20@schoolmath.eu>
2024-06-17 08:58:54 +0300

    View Available Shoe Models functionality implemented and completed in the GUI

--- CodeFirst-StorageControlApplication/GraphicalUserInterface/ViewShoeModelsForm.cs
+++ CodeFirst-StorageControlApplication/GraphicalUserInterface/ViewShoeModelsForm.cs
@@ -35,6 +35,7 @@ namespace GraphicalUserInterface
         {
             dataGridViewShoeModels = new DataGridView();
             btnClose = new Button();
+            label1 = new Label();
             ((System.ComponentModel.ISupportInitialize)dataGridViewShoeModels).BeginInit();
             SuspendLayout();
             // 
@@ -42,12 +43,11 @@ namespace GraphicalUserInterface
             // 
             dataGridViewShoeModels.BackgroundColor = Color.DodgerBlue;
             dataGridViewShoeModels.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewShoeModels.Location = new Point(393, 216);
+            dataGridViewShoeModels.Location = new Point(86, 187);
             dataGridViewShoeModels.Name = "dataGridViewShoeModels";
             dataGridViewShoeModels.RowHeadersWidth = 51;
-            dataGridViewShoeModels.Size = new Size(300, 188);
+            dataGridViewShoeModels.Size = new Size(909, 194);
             dataGridViewShoeModels.TabIndex = 0;
-            //dataGridViewShoeModels.Click += ViewShoeModelsForm_Load;
             // 
             // btnClose
             // 
@@ -59,15 +59,29 @@ namespace GraphicalUserInterface
             btnClose.UseVisualStyleBackColor = true;
             btnClose.Click += btnClose_Click;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Arial", 28.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label1.Location = new Point(200, 60);
+            label1.Name = "label1";
+            label1.Size = new Size(648, 55);
+            label1.TabIndex = 2;
+            label1.Text = "View Available Shoe Models";
+            // 
             // ViewShoeModelsForm
             // 
             BackgroundImage = Properties.Resources.bright_background_image;
             ClientSize = new Size(1074, 498);
+            Controls.Add(label1);
             Controls.Add(btnClose);
             Controls.Add(dataGridViewShoeModels);
+            MaximizeBox = false;
             Name = "ViewShoeModelsForm";
             ((System.ComponentModel.ISupportInitialize)dataGridViewShoeModels).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         private void btnClose_Click(object sender, EventArgs e)
@@ -76,6 +90,7 @@ namespace GraphicalUserInterface
         }
 
         private DataGridView dataGridViewShoeModels;
+        private Label label1;
         private Button btnClose;
     }
 }

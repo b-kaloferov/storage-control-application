62c17f682fa7c9f26bbc318b7c966fc1a7718e16
Dimitar25 <dimitar.bazlyankov@gmail.com>
2024-06-17 07:46:43 +0300

    Services in Main for GUI defined

--- CodeFirst-StorageControlApplication/GraphicalUserInterface/Program.cs
+++ CodeFirst-StorageControlApplication/GraphicalUserInterface/Program.cs
@@ -1,3 +1,7 @@
+using DataLayer;
+using Microsoft.EntityFrameworkCore;
+using ServiceLayer;
+
 namespace GraphicalUserInterface
 {
     internal static class Program
@@ -11,7 +15,26 @@ namespace GraphicalUserInterface
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            var optionsBuilder = new DbContextOptionsBuilder<StorageDbContext>();
+            optionsBuilder.UseSqlServer("Server=LAPTOP-UNHGGLSQ\\SQLEXPRESS;Database=StorageDb;Trusted_Connection=True;TrustServerCertificate=True;");
+            using (var storageDbContext = new StorageDbContext(optionsBuilder.Options))
+            {
+                var modelsContext = new ModelsContext(storageDbContext);
+                var modelService = new ModelService(modelsContext);
+
+                var clientsContext = new ClientsContext(storageDbContext);
+                var clientService = new ClientService(clientsContext);
+
+                var shoesContext = new ShoesContext(storageDbContext);
+                var shoeService = new ShoeService(shoesContext);
+
+                var orderDetailsContext = new OrderDetailsContext(storageDbContext);
+
+
+                var ordersContext = new OrdersContext(storageDbContext);
+                var orderService = new OrderService(ordersContext, orderDetailsContext, clientsContext, shoesContext);
+                Application.Run(new Form1(modelService));
+            }  
         }
     }
 }
